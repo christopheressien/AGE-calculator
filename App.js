6c1d0b44a590c948723a14eb8fc24f252ca6e5c4
Christopher Essien <essienchristopher.dev@gmail.com>
2025-07-10 14:28:31 +0100

    made some changes in JS

--- App.js
+++ App.js
@@ -13,14 +13,22 @@ displaybox.appendChild(newh1);
 let mytimer;
 
 calculatebtn.addEventListener("click", () => {
+  clearInterval(mytimer);
   const age = inputbox.value.trim();
   answer.style.display = "none";
   result.innerHTML = "";
 
   if (age === "") {
     newh1.innerHTML = "Please enter your age below...";
+    mytimer = setInterval(() => {
+      newh1.innerHTML = "";
+    }, 4000);
   } else if (isNaN(age)) {
     newh1.innerHTML = "Please enter a number...";
+    inputbox.value = "";
+    mytimer = setInterval(() => {
+      newh1.innerHTML = "";
+    }, 4000);
   } else {
     answer.style.display = "block";
     let remaining_years = 90 - age;
@@ -28,14 +36,9 @@ calculatebtn.addEventListener("click", () => {
     let formmated = remaining_weeks.toLocaleString();
     result.innerHTML = formmated;
     inputbox.value = "";
-    clearInterval(mytimer);
   }
 });
 
 resetbtn.addEventListener("click", () => {
   refresh.click();
 });
-
-mytimer = setInterval(() => {
-  newh1.innerHTML = "";
-}, 4000);
